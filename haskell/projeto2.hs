65a8247d6aa992d84610822efaa799c202825653
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-17 20:56:59 -0300

    Mudei nomes para ingles

--- haskell/projeto2.hs
+++ haskell/projeto2.hs
@@ -1,19 +1,20 @@
--- para usar o sort
 import Data.List
+import Data.Ord
 
 -- tipo Point tem o nome do vertice e sua lista de coordenadas
 data Point = Point String [Float]
 
 -- tipo Edge tem o nome dos dois vertices e a distancia entre eles
-data Edge = Edge String String Float deriving Show
+data Edge = Edge {distance :: Float, v1 :: String, v2 :: String} deriving (Show)
 
-main = print $ criaArestas [(Point "a" [1.0,2.0]), (Point "b" [3.0,4.0]), (Point "c" [5.0,6.0])]
+-- TODO: ordenar por distancia, crescente
+main = print $ createEdges [(Point "a" [1.0,2.0]), (Point "b" [3.0,4.0]), (Point "c" [5.0,6.0])]
 
-criaArestas :: [Point] -> [Edge]
-criaArestas listaPontos = [distanciaEntrePontos (Point p1 coord1) (Point p2 coord2) | (Point p1 coord1) <- listaPontos, (Point p2 coord2) <- listaPontos, p1 < p2]
+createEdges :: [Point] -> [Edge]
+createEdges listPoints = [distancePoints (Point p1 coord1) (Point p2 coord2) | (Point p1 coord1) <- listPoints, (Point p2 coord2) <- listPoints, p1 < p2]
 
-distanciaEntrePontos :: Point -> Point -> Edge
-distanciaEntrePontos (Point p1 coord1) (Point p2 coord2) = Edge p1 p2 (sqrt $ sum $ zipWith (subtracaoAoQuadrado) coord1 coord2)
+distancePoints :: Point -> Point -> Edge
+distancePoints (Point p1 coord1) (Point p2 coord2) = Edge (sqrt $ sum $ zipWith (subtractSquare) coord1 coord2) p1 p2
 
-subtracaoAoQuadrado :: Float -> Float -> Float
-subtracaoAoQuadrado a1 a2 = (a1 - a2) ** 2
+subtractSquare :: Float -> Float -> Float
+subtractSquare f1 f2 = (f1 - f2) ** 2
