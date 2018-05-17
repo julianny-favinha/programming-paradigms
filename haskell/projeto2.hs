d6571ad5113ae0d5366085790f47360266475d04
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-17 20:26:57 -0300

    Calcula a distancia entre os vertices e cria as arestas

--- haskell/projeto2.hs
+++ haskell/projeto2.hs
@@ -1,5 +1,19 @@
-import Data.Char  
-  
-main = do  
-    contents <- getContents  
-    putStr (map toUpper contents)
+-- para usar o sort
+import Data.List
+
+-- tipo Point tem o nome do vertice e sua lista de coordenadas
+data Point = Point String [Float]
+
+-- tipo Edge tem o nome dos dois vertices e a distancia entre eles
+data Edge = Edge String String Float deriving Show
+
+main = print $ criaArestas [(Point "a" [1.0,2.0]), (Point "b" [3.0,4.0]), (Point "c" [5.0,6.0])]
+
+criaArestas :: [Point] -> [Edge]
+criaArestas listaPontos = [distanciaEntrePontos (Point p1 coord1) (Point p2 coord2) | (Point p1 coord1) <- listaPontos, (Point p2 coord2) <- listaPontos, p1 < p2]
+
+distanciaEntrePontos :: Point -> Point -> Edge
+distanciaEntrePontos (Point p1 coord1) (Point p2 coord2) = Edge p1 p2 (sqrt $ sum $ zipWith (subtracaoAoQuadrado) coord1 coord2)
+
+subtracaoAoQuadrado :: Float -> Float -> Float
+subtracaoAoQuadrado a1 a2 = (a1 - a2) ** 2
