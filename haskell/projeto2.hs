e19efeb7337feb47f07a23d552bef1705b7e3ccf
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-17 21:00:38 -0300

    Ordenando array de Edges por distancia

--- haskell/projeto2.hs
+++ haskell/projeto2.hs
@@ -5,10 +5,9 @@ import Data.Ord
 data Point = Point String [Float]
 
 -- tipo Edge tem o nome dos dois vertices e a distancia entre eles
-data Edge = Edge {distance :: Float, v1 :: String, v2 :: String} deriving (Show)
+data Edge = Edge Float String String deriving (Show, Ord, Eq)
 
--- TODO: ordenar por distancia, crescente
-main = print $ createEdges [(Point "a" [1.0,2.0]), (Point "b" [3.0,4.0]), (Point "c" [5.0,6.0])]
+main = print $ sort $ createEdges [(Point "a" [1.0,2.0]), (Point "b" [3.0,4.0]), (Point "c" [5.0,6.0])]
 
 createEdges :: [Point] -> [Edge]
 createEdges listPoints = [distancePoints (Point p1 coord1) (Point p2 coord2) | (Point p1 coord1) <- listPoints, (Point p2 coord2) <- listPoints, p1 < p2]
