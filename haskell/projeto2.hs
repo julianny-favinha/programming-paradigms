c501f74e9d667a745c61f5d5d35947c43553c358
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-21 17:20:29 -0300

    Input funcionando

--- haskell/projeto2.hs
+++ haskell/projeto2.hs
@@ -1,13 +1,13 @@
 -- para usar o sort
 import Data.List
+import Prelude
 
 -- tipo Point tem o nome do vertice e sua lista de coordenadas
-data Point = Point String [Float]
+data Point = Point String [Float] deriving Show
 
 -- tipo Edge tem o nome dos dois vertices e a distancia entre eles
 data Edge = Edge Float String String deriving (Show, Ord, Eq)
 
-main = print $ sort $ createEdges [(Point "a" [1.0,2.0]), (Point "b" [3.0,4.0]), (Point "c" [5.0,6.0])]
 
 createEdges :: [Point] -> [Edge]
 createEdges listPoints = [distancePoints (Point p1 coord1) (Point p2 coord2) | (Point p1 coord1) <- listPoints, (Point p2 coord2) <- listPoints, p1 < p2]
@@ -17,3 +17,33 @@ distancePoints (Point p1 coord1) (Point p2 coord2) = Edge (sqrt $ sum $ zipWith 
 
 subtractSquare :: Float -> Float -> Float
 subtractSquare f1 f2 = (f1 - f2) ** 2
+
+
+-- convert single param to int
+rFloat :: String -> Float
+rFloat = read
+
+rInt :: String -> Int
+rInt = read
+
+-- converting lines to int
+convertAux :: [String] -> [Float]
+convertAux y = map rFloat y
+
+convert :: [String] -> Point
+convert line = do 
+    let
+      h = head line -- head is not int
+      r = tail line
+      coords = convertAux r
+    (Point h coords)
+
+main = do 
+    n <- getLine
+    c <- getContents
+    let 
+        l = lines c 
+        w = map words l
+        points = map convert w
+        
+    print $ (sort $ createEdges $ points)
