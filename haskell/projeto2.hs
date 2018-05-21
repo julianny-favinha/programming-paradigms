531eafba04af3811af27890fedd86362fc15f00c
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-21 19:13:45 -0300

    Mudei pra vertex o points, pra ficar mais claro

--- haskell/projeto2.hs
+++ haskell/projeto2.hs
@@ -2,42 +2,45 @@
 import Data.List
 import Prelude
 
--- tipo Point tem o nome do vertice e sua lista de coordenadas
-data Point = Point String [Float] deriving Show
+-- tipo Vertex tem o nome do vertice e sua lista de coordenadas
+data Vertex = Vertex String [Float] deriving Show
 
 -- tipo Edge tem o nome dos dois vertices e a distancia entre eles
 data Edge = Edge Float String String deriving (Show, Ord, Eq)
 
-data Graph = Graph [Point] [Edge] deriving (Show)
+data Graph = Graph [Vertex] [Edge] deriving (Show)
 
--- cria lista de Edges a partir da distancia entre Points
-createEdges :: [Point] -> [Edge]
-createEdges listPoints = [distancePoints (Point p1 coord1) (Point p2 coord2) | (Point p1 coord1) <- listPoints, (Point p2 coord2) <- listPoints, p1 < p2]
+-- cria lista de Edges a partir da distancia entre Vertex
+createEdges :: [Vertex] -> [Edge]
+createEdges listVertex = [distanceVertex (Vertex p1 coord1) (Vertex p2 coord2) | (Vertex p1 coord1) <- listVertex, (Vertex p2 coord2) <- listVertex, p1 < p2]
 
--- calcula distancia entre Points
-distancePoints :: Point -> Point -> Edge
-distancePoints (Point p1 coord1) (Point p2 coord2) = Edge (sqrt $ sum $ zipWith (\ f1 f2 -> (f1 - f2)**2) coord1 coord2) p1 p2
+-- calcula distancia entre Vertex
+distanceVertex :: Vertex -> Vertex -> Edge
+distanceVertex (Vertex p1 coord1) (Vertex p2 coord2) = Edge (sqrt $ sum $ zipWith (\ f1 f2 -> (f1 - f2)**2) coord1 coord2) p1 p2
 
--- converte uma linha do input em Point
-convert :: [String] -> Point
+-- converte uma linha do input em Vertex
+convert :: [String] -> Vertex
 convert line = do 
     let
       nome = head line
       coords = map read (tail line)
-    (Point nome coords)
+    (Vertex nome coords)
 
 -- remove os n primeiros elementos de uma lista
 drop' :: Int -> [a] -> [a]
-drop' n xs = [ v | (_,v)  <- filter (\(x,_) -> x > n) $ zip [1..] xs ]
+drop' n xs = [v | (_,v) <- filter (\(x,_) -> x > n) $ zip [1..] xs]
 
--- ************* MAIN *************
+-- dado uma lista de pontos (vertices), uma lista de arestas ordenada cresc, retorna a AGM (lista de arestas)
+--kruskal :: [Vertex] -> [Edge] -> [Edge]
+
+-- ******************* MAIN *******************
 main = do 
     k <- getLine
     c <- getContents
     let 
         l = lines c 
         w = map words l
-        points = map convert w
+        vertex = map convert w
         
-    print $ drop' ((read k)-1) (reverse $ sort $ createEdges $ points)
+    print $ drop' ((read k)-1) (reverse $ sort $ createEdges $ vertex)
     --print $ kruskal (sort $ createEdges $ points)
