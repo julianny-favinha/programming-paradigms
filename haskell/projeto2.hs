6c53607af22a4e0bb751cc6e917361dff471811a
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-21 19:32:30 -0300

    Algoritmo da internet para kruskal... testar melhor

--- haskell/projeto2.hs
+++ haskell/projeto2.hs
@@ -8,6 +8,15 @@ data Vertex = Vertex String [Float] deriving Show
 -- tipo Edge tem o nome dos dois vertices e a distancia entre eles
 data Edge = Edge Float String String deriving (Show, Ord, Eq)
 
+edgeSrc :: Edge -> String
+edgeSrc (Edge _ a _) = a
+
+edgeDest :: Edge -> String
+edgeDest (Edge _ _ b) = b
+
+edgeWeight :: Edge -> Float
+edgeWeight (Edge d _ _) = d
+
 data Graph = Graph [Vertex] [Edge] deriving (Show)
 
 -- cria lista de Edges a partir da distancia entre Vertex
@@ -30,6 +39,51 @@ convert line = do
 drop' :: Int -> [a] -> [a]
 drop' n xs = [v | (_,v) <- filter (\(x,_) -> x > n) $ zip [1..] xs]
 
+kruskal :: [Edge] -> [Edge]
+kruskal g = kruskalRec g [] [ (x, x) | x <- (points g)]
+
+kruskalRec :: [Edge] -> [Edge] -> [(String, String)] -> [Edge]
+kruskalRec [] progress sets = progress
+kruskalRec (e:es) progress sets
+    | joined (edgeSrc e) (edgeDest e) sets = kruskalRec es progress sets
+    | otherwise = kruskalRec es (e : progress) (join (edgeSrc e) (edgeDest e) sets)
+
+parent :: String -> [(String, String)] -> String
+parent elem sets
+    |fst match == snd match = fst match
+    |otherwise = parent (snd match) sets
+    where
+        match = head ([ (x, y) |(x, y) <- sets, x == elem]) 
+
+joined :: String -> String -> [(String, String)] -> Bool
+joined x y sets = (parent x sets) == (parent y sets)
+
+join :: String -> String -> [(String, String)] -> [(String, String)]
+join x y sets
+    | joined x y sets = sets
+    | otherwise = newY : rest
+    where
+        newY = (fst oldY, x)
+        oldY = head [(a, b) | (a, b) <- sets, a==(parent y sets)]
+        rest = [(a, b) | (a, b) <- sets, a /= (parent y sets)]
+
+points :: [Edge] -> [String]
+points g = pointsRec g []
+
+pointsRec :: [Edge] -> [String] -> [String]
+pointsRec [] s = s
+pointsRec (e:es) s = pointsRec es (first ++ second ++ s) where
+    first
+        | not(elem (edgeSrc e) s ) = [edgeSrc e]
+        | otherwise = []
+    second
+        | edgeSrc e == edgeDest e = []
+        | not(elem (edgeDest e) s) = [edgeDest e]
+        | otherwise = []
+
+-- ********************************************************* TODO *********************************************************
+--procuraGrupos :: [Vertex] -> [Edge] -> [[Vertex]]
+
 -- ******************* MAIN *******************
 main = do 
     k <- getLine
@@ -39,5 +93,4 @@ main = do
         w = map words l
         vertex = map convert w
         
-    print $ drop' ((read k)-1) (reverse $ sort $ createEdges $ vertex)
-    --print $ kruskal (sort $ createEdges $ points)
+    print $ drop' ((read k)-1) (reverse $ sort $ kruskal (sort $ createEdges $ vertex))
