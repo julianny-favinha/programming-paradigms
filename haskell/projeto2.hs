c6677799d20eb80ff0019a6ea0acdc69c1c45057
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-21 19:05:42 -0300

    Melhorei funcoes e comentei. arquivo dados recebe k como primeira linha, nao a quantidade de pontos

--- haskell/projeto2.hs
+++ haskell/projeto2.hs
@@ -8,42 +8,36 @@ data Point = Point String [Float] deriving Show
 -- tipo Edge tem o nome dos dois vertices e a distancia entre eles
 data Edge = Edge Float String String deriving (Show, Ord, Eq)
 
+data Graph = Graph [Point] [Edge] deriving (Show)
 
+-- cria lista de Edges a partir da distancia entre Points
 createEdges :: [Point] -> [Edge]
 createEdges listPoints = [distancePoints (Point p1 coord1) (Point p2 coord2) | (Point p1 coord1) <- listPoints, (Point p2 coord2) <- listPoints, p1 < p2]
 
+-- calcula distancia entre Points
 distancePoints :: Point -> Point -> Edge
-distancePoints (Point p1 coord1) (Point p2 coord2) = Edge (sqrt $ sum $ zipWith (subtractSquare) coord1 coord2) p1 p2
+distancePoints (Point p1 coord1) (Point p2 coord2) = Edge (sqrt $ sum $ zipWith (\ f1 f2 -> (f1 - f2)**2) coord1 coord2) p1 p2
 
-subtractSquare :: Float -> Float -> Float
-subtractSquare f1 f2 = (f1 - f2) ** 2
-
-
--- convert single param to int
-rFloat :: String -> Float
-rFloat = read
-
-rInt :: String -> Int
-rInt = read
-
--- converting lines to int
-convertAux :: [String] -> [Float]
-convertAux y = map rFloat y
-
+-- converte uma linha do input em Point
 convert :: [String] -> Point
 convert line = do 
     let
-      h = head line -- head is not int
-      r = tail line
-      coords = convertAux r
-    (Point h coords)
+      nome = head line
+      coords = map read (tail line)
+    (Point nome coords)
 
+-- remove os n primeiros elementos de uma lista
+drop' :: Int -> [a] -> [a]
+drop' n xs = [ v | (_,v)  <- filter (\(x,_) -> x > n) $ zip [1..] xs ]
+
+-- ************* MAIN *************
 main = do 
-    n <- getLine
+    k <- getLine
     c <- getContents
     let 
         l = lines c 
         w = map words l
         points = map convert w
         
-    print $ (sort $ createEdges $ points)
+    print $ drop' ((read k)-1) (reverse $ sort $ createEdges $ points)
+    --print $ kruskal (sort $ createEdges $ points)
